813dd7ace22741bef70175fb1259177aab26b826
Martin <hello@northrook.com>
2024-11-13 06:07:31 +0000

    mend

---
+++ src/DependencyInjection/ServiceContainerInterface.php
@@ -0,0 +1,22 @@
+<?php
+
+declare( strict_types = 1 );
+
+namespace Core\Symfony\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ServiceLocator;
+use Symfony\Contracts\Service\Attribute\Required;
+use Symfony\Contracts\Service\ServiceCollectionInterface;
+
+interface ServiceContainerInterface
+{
+    /**
+     * @template T of ServiceCollectionInterface
+     *
+     * @param ServiceLocator<T>  $serviceLocator
+     *
+     * @return void
+     */
+    #[Required]
+    public function setServiceLocator( ServiceLocator $serviceLocator ) : void;
+}
--- src/EventListener/ResponseEventListener.php
+++ src/EventListener/ResponseEventListener.php
@@ -2,4 +2,41 @@
 
 namespace Core\Symfony\EventListener;
 
-class ResponseEventListener {}
+use Core\Symfony\DependencyInjection\ServiceContainerInterface;
+use Northrook\Clerk;
+use Northrook\Logger\Log;
+use Symfony\Component\HttpFoundation\Request;
+use function Support\get_class_name;
+
+abstract class ResponseEventListener
+{
+
+    public function __construct()
+    {
+        Clerk::event(__METHOD__, $this::class);
+    }
+
+    /**
+     * Check if the passed {@see Request} is using a controller implementing the {@see ServiceContainerInterface}.
+     *
+     * @param Request  $request
+     *
+     * @return bool
+     */
+    final protected function handleController(Request $request) : bool
+    {
+        Clerk::event(__METHOD__, $this::class);
+
+        $_controller = $request->attributes->get('_controller');
+
+        if (!\is_string($_controller)) {
+            Log::warning(
+                '{method} Controller attribute was expected be a string. Returning {false}.',
+                [ 'method' => __METHOD__ ],
+            );
+            return false;
+        }
+
+        return \is_subclass_of(get_class_name($_controller, true), ServiceContainerInterface::class);
+    }
+}
