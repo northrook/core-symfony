d98a7282a4f6dc668073e1f95761afff436940ea
Martin <hello@northrook.com>
2024-11-13 09:58:00 +0000

    mend

--- src/EventListener/ResponseEventListener.php
+++ src/EventListener/ResponseEventListener.php
@@ -10,6 +10,7 @@ use function Support\get_class_name;
 
 abstract class ResponseEventListener implements ServiceContainerInterface
 {
+    protected readonly ?string $controller;
 
     // TODO : Provide an in-memory/file cache for handleController and other simple calls
     public function __construct()
@@ -28,22 +29,23 @@ abstract class ResponseEventListener implements ServiceContainerInterface
     {
         Clerk::event(__METHOD__, $this::class);
 
-        $_controller = $request->attributes->get('_controller');
+        if (!isset($this->controller)) {
+            $_controller = $request->attributes->get('_controller');
 
-        if (!\is_string($_controller)) {
-            Log::warning(
-                '{method} Controller attribute was expected be a string. Returning {false}.',
-                [ 'method' => __METHOD__ ],
-            );
+            if (!\is_string($_controller)) {
+                Log::warning(
+                    '{method} Controller attribute was expected be a string. Returning {false}.',
+                    [ 'method' => __METHOD__ ],
+                );
+                return false;
+            }
+            $this->controller = get_class_name($_controller);
+        }
+
+        if (!$this->controller || !class_exists($this->controller)) {
             return false;
         }
 
-        $controller = get_class_name($_controller);
-
-        if (!$controller || !class_exists($controller)) {
-            return false;
-        }
-
-        return \is_subclass_of($controller, ServiceContainerInterface::class);
+        return \is_subclass_of($this->controller, ServiceContainerInterface::class);
     }
 }
