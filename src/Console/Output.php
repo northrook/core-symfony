208daa797fe8e424e1166c23382287be7a31fcf1
Martin <hello@northrook.com>
2024-11-17 15:01:42 +0000

    mend

--- src/Console/Output.php
+++ src/Console/Output.php
@@ -1,44 +1,100 @@
 <?php
 
+declare(strict_types = 1);
+
 namespace Core\Symfony\Console;
 
-use Symfony\Component\Console\Formatter\OutputFormatterInterface;
 use Symfony\Component\Console\Input\StringInput;
-use Symfony\Component\Console\Output\BufferedOutput;
 use Symfony\Component\Console\Output\ConsoleOutput;
-use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
-/**
- * @method static block( string|array $messages, ?string $type = null, ?string $style = null, string $prefix = ' ', bool $padding = false, bool $escape = true ): void
- * @method static title( string $message ): void
- * @method static section( string $message ): void
- * @method static listing( array $elements ): void
- * @method static text( string|array $message ): void
- * @method static comment( string|array $message ): void
- * @method static success( string|array $message ): void
- * @method static error( string|array $message ): void
- * @method static warning( string|array $message ): void
- * @method static note( string|array $message ): void
- * @method static info( string|array $message ): void
- * @method static caution( string|array $message ): void
- */
-final class Output extends SymfonyStyle
+final class Output
 {
-    private static Output $instance;
+    private static SymfonyStyle $instance;
 
-    public function __construct()
+    /**
+     * @param string  ...$message
+     *
+     * @return void
+     */
+    public static function text(string ...$message) : void
     {
-        parent::__construct( new StringInput( null ), new ConsoleOutput() );
+        Output::print()->text($message);
     }
 
-    public static function __callStatic( string $name, array $arguments ) : void
+    /**
+     * @param string  ...$message
+     *
+     * @return void
+     */
+    public static function comment(string ...$message) : void
     {
-        if ( !\method_exists( static::class, $name ) ) {
-            throw new \BadMethodCallException( static::class . '::' . $name . '() does not exist' );
-        }
+        Output::print()->comment($message);
+    }
 
-        ( Output::$instance ??= new Output() )->$name( ...$arguments );
+    /**
+     * @param string  ...$message
+     *
+     * @return void
+     */
+    public static function success(string ...$message) : void
+    {
+        Output::print()->success($message);
+    }
+
+    /**
+     * @param string  ...$message
+     *
+     * @return void
+     */
+    public static function error(string ...$message) : void
+    {
+        Output::print()->error($message);
+    }
+
+    /**
+     * @param string  ...$message
+     *
+     * @return void
+     */
+    public static function warning(string ...$message) : void
+    {
+        Output::print()->warning($message);
+    }
+
+    /**
+     * @param string  ...$message
+     *
+     * @return void
+     */
+    public static function note(string ...$message) : void
+    {
+        Output::print()->note($message);
+    }
+
+    /**
+     * @param string  ...$message
+     *
+     * @return void
+     */
+    public static function info(string ...$message) : void
+    {
+        Output::print()->info($message);
+    }
+
+    /**
+     * @param string  ...$message
+     *
+     * @return void
+     */
+    public static function caution(string ...$message) : void
+    {
+        Output::print()->caution($message);
+    }
+
+    public static function print() : SymfonyStyle
+    {
+        return self::$instance ??= new SymfonyStyle(new StringInput(''), new ConsoleOutput());
     }
 
 }
