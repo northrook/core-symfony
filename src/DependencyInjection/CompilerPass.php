cf8f43c2243fa0593d52d39e835030738f092145
Martin <hello@northrook.com>
2024-11-29 13:18:02 +0000

    mend

--- src/DependencyInjection/CompilerPass.php
+++ src/DependencyInjection/CompilerPass.php
@@ -1,11 +1,12 @@
-<?php /** @noinspection PhpClassCanBeReadonlyInspection */
+<?php
+
+/** @noinspection PhpClassCanBeReadonlyInspection */
 
 namespace Core\Symfony\DependencyInjection;
 
 use Northrook\Filesystem\Path;
 use JetBrains\PhpStorm\Language;
-use Support\Normalize;
-use Support\Time;
+use Support\{Normalize, Time};
 use Symfony\Component\Console\Input\StringInput;
 use Symfony\Component\Console\Output\ConsoleOutput;
 use Symfony\Component\Console\Style\SymfonyStyle;
@@ -20,117 +21,118 @@ use UnexpectedValueException;
  */
 abstract class CompilerPass implements CompilerPassInterface
 {
+    public const array PLACEHOLDER_ARGS = [[]];
+
+    public const array PLACEHOLDER_ARG = [];
+
     protected readonly string $projectDirectory;
 
     protected readonly ParameterBagInterface $parameterBag;
 
     protected readonly SymfonyStyle $console;
 
-    abstract public function compile(ContainerBuilder $container) : void;
+    abstract public function compile( ContainerBuilder $container ) : void;
 
-    final public function process(ContainerBuilder $container) : void
+    final public function process( ContainerBuilder $container ) : void
     {
-        $this->console          = new SymfonyStyle(new StringInput(''), new ConsoleOutput());
+        $this->console          = new SymfonyStyle( new StringInput( '' ), new ConsoleOutput() );
         $this->parameterBag     = $container->getParameterBag();
         $this->projectDirectory = $this->setProjectDirectory();
 
-        $this->compile($container);
+        $this->compile( $container );
     }
 
-    protected function path(string $fromProjectDir) : Path
+    protected function path( string $fromProjectDir ) : Path
     {
-        return new Path("{$this->projectDirectory}/{$fromProjectDir}");
+        return new Path( "{$this->projectDirectory}/{$fromProjectDir}" );
     }
 
     /**
-     * @param string                          $fromProjectDir
-     * @param array<array-key, mixed>|string  $data
-     * @param bool                            $override
+     * @param string                         $fromProjectDir
+     * @param array<array-key, mixed>|string $data
+     * @param bool                           $override
      *
      * @return void
      */
     final protected function createYamlFile(
-        string         $fromProjectDir,
-        #[Language('PHP')]
-        string | array $data,
-        bool           $override = false,
+        string       $fromProjectDir,
+        #[Language( 'PHP' )] string|array $data,
+        bool         $override = false,
     ) : void {
-        $path = $this->path($fromProjectDir);
+        $path = $this->path( $fromProjectDir );
 
-        if ($path->exists() && false === $override) {
+        if ( $path->exists() && false === $override ) {
             return;
         }
 
-        $path->save(Yaml::dump($data));
+        $path->save( Yaml::dump( $data ) );
     }
 
     final protected function createPhpFile(
-        string $fromProjectDir,
-        #[Language('PHP')]
-        string $php,
-        bool   $override = false,
+        string    $fromProjectDir,
+        #[Language( 'PHP' )] string    $php,
+        bool      $override = false,
         string ...$comment,
     ) : void {
-        $path = $this->path($fromProjectDir);
+        $path = $this->path( $fromProjectDir );
 
-        if ($path->exists() && false === $override) {
+        if ( $path->exists() && false === $override ) {
             return;
         }
 
-        $path->save($this->parsePhpString($php, ...$comment));
+        $path->save( $this->parsePhpString( $php, ...$comment ) );
     }
 
     private function setProjectDirectory() : string
     {
-        $projectDirectory = $this->parameterBag->get('kernel.project_dir');
+        $projectDirectory = $this->parameterBag->get( 'kernel.project_dir' );
 
         \assert(
-            \is_string($projectDirectory)
-                && \is_dir($projectDirectory)
-                && \is_writable($projectDirectory),
+            \is_string( $projectDirectory )
+                && \is_dir( $projectDirectory )
+                && \is_writable( $projectDirectory ),
         );
 
-        return Normalize::path($projectDirectory);
+        return Normalize::path( $projectDirectory );
     }
 
     private function parsePhpString(
-        #[Language('PHP')]
-        string $php,
+        #[Language( 'PHP' )] string    $php,
         string ...$comment,
     ) : string {
-        if (!\str_starts_with($php, '<?php')) {
-            throw new UnexpectedValueException(__METHOD__ . ': The provided PHP string has no opening tag.');
+        if ( ! \str_starts_with( $php, '<?php' ) ) {
+            throw new UnexpectedValueException( __METHOD__.': The provided PHP string has no opening tag.' );
         }
 
-        if (\str_ends_with($php, '?>')) {
-            throw new UnexpectedValueException(__METHOD__ . ': PHP strings must not end with a closing tag.');
+        if ( \str_ends_with( $php, '?>' ) ) {
+            throw new UnexpectedValueException( __METHOD__.': PHP strings must not end with a closing tag.' );
         }
 
-        $generator = '    This file is autogenerated by ' . $this::class . '.';
-        $generated = '    Date: ' . (new Time())->datetime;
+        $generator = '    This file is autogenerated by '.$this::class.'.';
+        $generated = '    Date: '.( new Time() )->datetime;
 
-        $separator = \str_repeat('-', \strlen($generator));
+        $separator = \str_repeat( '-', \strlen( $generator ) );
 
         $header   = [];
         $header[] = "\n\n/*{$separator}\n";
         $header[] = $generator;
         $header[] = $generated;
-        if ($comment) {
+        if ( $comment ) {
             $header[] = '';
 
-            foreach ($comment as $line) {
-                $header[] = '    ' . $line;
+            foreach ( $comment as $line ) {
+                $header[] = '    '.$line;
             }
         }
         $header[] = "\n{$separator}*/\n\n";
 
         $content = \preg_replace(
             pattern     : '#<\?php\s+?(?=\S)#A',
-            replacement : '<?php' . \implode("\n", $header),
+            replacement : '<?php'.\implode( "\n", $header ),
             subject     : $php,
         );
 
-        \assert(\is_string($content));
+        \assert( \is_string( $content ) );
 
         return $content;
     }
