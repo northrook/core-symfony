58b94ac6ec190f87a398e68343d6e02fb89e8a07
Martin <hello@northrook.com>
2024-12-05 14:11:32 +0000

    mend

--- src/EventListener/HttpEventListener.php
+++ src/EventListener/HttpEventListener.php
@@ -9,9 +9,15 @@ use InvalidArgumentException;
 use Northrook\Clerk;
 use Northrook\Logger\Log;
 use Core\Symfony\DependencyInjection\{ServiceContainer, ServiceContainerInterface};
+use Symfony\Component\Cache\Adapter\{ArrayAdapter};
+use Support\Normalize;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Closure;
+use Throwable;
 use Symfony\Component\HttpKernel\Event\{ExceptionEvent, KernelEvent};
-use function Support\{explode_class_callable, get_class_id};
+use Symfony\Contracts\Cache\{CacheInterface, ItemInterface};
+use function Support\{classBasename, explode_class_callable, get_class_id};
+use function String\hashKey;
 
 /**
  * @author Martin Nielsen <mn@northrook.com>
@@ -20,19 +26,27 @@ abstract class HttpEventListener implements EventSubscriberInterface, ServiceCon
 {
     use ServiceContainer;
 
-    protected string $eventId;
+    private readonly CacheInterface $httpEventCache;
 
     protected readonly string $listenerId;
 
-    /** @var class-string|false The `Controller` used */
+    /** @var class-string|false The `Controller` used. */
     protected string|false $controller;
 
-    /** @var false|string The `Controller::method` called */
+    /** @var false|string The `Controller::method` called. */
     protected string|false $action;
 
-    public function __construct( protected readonly Clerk $clerk )
-    {
-        $this->listenerId = get_class_id( $this );
+    /** @var string the current `_route` name */
+    protected string $route;
+
+    public function __construct(
+        protected readonly Clerk $clerk,
+        ?CacheInterface          $cache = null,
+    ) {
+        if ( $cache ) {
+            $this->httpEventCache = $cache;
+        }
+        $this->listenerId = classBasename( $this::class );
         $this->clerk::event( $this->listenerId, 'http' );
         Log::notice( $this->listenerId.' does this adopt [monolog.tags]?' );
     }
@@ -45,12 +59,27 @@ abstract class HttpEventListener implements EventSubscriberInterface, ServiceCon
      */
     final protected function shouldSkip( KernelEvent $event, array $skip = [ExceptionEvent::class] ) : bool
     {
-        $eventId = __METHOD__.'::'.\spl_object_id( $event );
-
+        $eventId = 'HttpEventListener::shouldSkip( '.classBasename( $event ).' )';
         $this->clerk::event( $eventId, 'http' );
 
-        [$this->controller, $this->action] = Cache\memoize(
-            callback    : function() use ( $skip, $event ) : array {
+        // Only parse GET requests
+        if ( ! $event->getRequest()->isMethod( 'GET' ) ) {
+            return true;
+        }
+
+        $this->route = (string) $event->getRequest()->attributes->get( '_route', '' );
+
+        if ( ! $this->route ) {
+            Log::alert(
+                'Expected a {_route} parameter, but none was found.',
+                ['requestAttributes' => $event->getRequest()->attributes->all()],
+            );
+            return true;
+        }
+
+        [$this->controller, $this->action] = $this->cache(
+            'shouldShip.'.hashKey( $skip ),
+            function() use ( $skip, $event ) : array {
                 // Check if the `$event` itself should be skipped outright.
                 foreach ( $skip as $kernelEvent ) {
                     if ( $event instanceof $kernelEvent ) {
@@ -89,18 +118,58 @@ abstract class HttpEventListener implements EventSubscriberInterface, ServiceCon
                 }
                 return [false, false];
             },
-            key         : Cache\key(
-                [
-                    __METHOD__,
-                    $event->getRequest()->attributes->get( '_route', $eventId ),
-                    ...$skip,
-                ],
-            ),
-            persistence : Cache\FOREVER,
         );
 
         $this->clerk::stop( $eventId );
 
         return ! $this->controller;
+    }
+
+    /**
+     * @template Type
+     *
+     * @param string         $key         Key - a hash based on $callback and $arguments will be used if null
+     * @param Closure():Type $callback    a function or method to cache, optionally with extra arguments as array values
+     * @param ?int           $persistence the duration in seconds for the cache entry
+     *
+     * @return Type
+     * @phpstan-return Type
+     */
+    final protected function cache( string $key, Closure $callback, ?int $persistence = Cache\AUTO ) : mixed
+    {
+        $this->httpEventCache ??= new ArrayAdapter(
+            0,
+            false,
+            14_400,
+            1_024,
+        );
+
+        \assert(
+            \ctype_alnum( \str_replace( '.', '', $key ) ),
+            'The '.__METHOD__.'( $key .. ) can only contain letters, numbers, and periods.',
+        );
+
+        $key = Normalize::key( [$this->route, $key] );
+
+        try {
+            return $this->httpEventCache->get(
+                key      : Normalize::key( [$this->route, $key] ),
+                callback : static function( ItemInterface $memo ) use ( $callback, $persistence ) : mixed {
+                    $memo->expiresAfter( $persistence );
+                    return $callback();
+                },
+            );
+        }
+        catch ( Throwable $exception ) {
+            Log::error(
+                'Exception thrown when using {runtime}: {message}.',
+                [
+                    'runtime'   => $this::class,
+                    'message'   => $exception->getMessage(),
+                    'exception' => $exception,
+                ],
+            );
+        }
+        return $callback();
     }
 }
