87dcb594d240dea7207ffd44d704b37aae11714c
Martin <hello@northrook.com>
2025-01-22 20:57:43 +0000

    mend

--- src/Interface/FilterInterface.php
+++ src/Interface/FilterInterface.php
@@ -5,7 +5,14 @@ declare(strict_types=1);
 namespace Core\Symfony\Interface;
 
 /**
- * @method void  callback()
- * @method mixed filter( mixed $filter )
+ * Provides a `method( callback )` argument for {@see Definition::addMethodCall()}
  */
-interface FilterInterface {}
+interface FilterInterface
+{
+    /**
+     * @param string $method
+     *
+     * @return array{string, array{...}}
+     */
+    public static function callback( string $method ) : array;
+}
