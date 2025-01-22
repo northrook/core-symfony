ecca4a2d36acb1e249488d84e369f63b80bf7d00
Martin <hello@northrook.com>
2025-01-22 20:14:43 +0000

    mend

---
+++ src/Interface/FilterInterface.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Core\Symfony\Interface;
+
+/**
+ * @method void  callback()
+ * @method mixed filter( mixed $filter )
+ */
+interface FilterInterface {}
