efb782c5e2061dc9cc8c38cecdfd56cf8fc1dcdb
Martin <hello@northrook.com>
2025-02-16 11:11:42 +0000

    mend

--- src/Config/Argument.php
+++ src/Config/Argument.php
@@ -5,8 +5,15 @@ declare(strict_types=1);
 namespace Core\Symfony\Config;
 
 use Core\Symfony\Interface\ArgumentInterface;
+use JetBrains\PhpStorm\Deprecated;
 
+#[Deprecated]
 abstract class Argument implements ArgumentInterface
 {
+    /**
+     * @param string $method
+     *
+     * @return array<string, callable>
+     */
     abstract public static function call( string $method ) : array;
 }
