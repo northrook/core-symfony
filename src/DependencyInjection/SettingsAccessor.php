062e6b4658fb095a907f2c51c4b5a038f5a96a0c
Martin <hello@northrook.com>
2025-03-09 08:38:38 +0000

    mend

--- src/DependencyInjection/SettingsAccessor.php
+++ src/DependencyInjection/SettingsAccessor.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Core\Symfony\DependencyInjection;
 
+use JetBrains\PhpStorm\Deprecated;
+
 trait SettingsAccessor
 {
     // protected SettingsProvider $settings;
@@ -12,6 +14,30 @@ trait SettingsAccessor
         'auth.onboarding' => true,
     ];
 
+    /**
+     * @internal
+     *
+     * @param string                               $key
+     * @param null|array<array-key, scalar>|scalar $default
+     *
+     * @return null|array<array-key, scalar>|scalar
+     */
+    final protected function getSetting(
+        string                           $key,
+        null|array|bool|float|int|string $default,
+    ) : null|array|bool|float|int|string {
+        // TODO: [m] Use a logger or dump statement here to sniff out all desired settings
+        return self::DEFAULTS[$key] ?? $default;
+    }
+
+    /**
+     * @internal
+     *
+     * @param string $get
+     *
+     * @return bool|string
+     */
+    #[Deprecated( 'Use getSetting() instead')]
     final protected function settings( string $get ) : string|bool
     {
         return self::DEFAULTS[$get] ?? false;
